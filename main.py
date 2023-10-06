5be12157b3986885c77fb79648e23b059d28571c
Olumide Daramola <olumidedaramola426@gmail.com>
2023-10-06 16:47:07 +0100

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
@@ -6,9 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello_world!"
+    today = datetime.today().strftime("%B %#d, %Y")
+    return render_template("invoice.html", date=today)
 
 
-if __name__ == "name":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
